f3ac9f50f144662e8792c74a1d484044447e19fc
Anna <annachen368@gmail.com>
2016-04-26 23:55:27 -0700

    setOnQueryTextListener finished

--- app/src/main/java/com/example/anna/placesearching/MainActivity.java
+++ app/src/main/java/com/example/anna/placesearching/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.anna.placesearching;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.util.Pools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Hi", Toast.LENGTH_LONG).show();
         }
     }
-
-
+    private static final String[] itemArrayList = new String[] { "Belgium",
+            "France", "France_", "Italy", "Germany", "Spain" };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the options menu from XML
@@ -44,6 +62,114 @@ public class MainActivity extends AppCompatActivity {
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         //searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
+        // autocomplete
+        SearchView.SearchAutoComplete searchAutoComplete =
+                (SearchView.SearchAutoComplete)  searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchAutoComplete.setTextColor(Color.WHITE);
+        searchAutoComplete.setDropDownBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.abc_popup_background_mtrl_mult));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, itemArrayList);
+        searchAutoComplete.setAdapter(adapter);
+
+        // check if users type something
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.i("text", "hi");
+
+                // download places
+                DownloadTask downloadTask = new DownloadTask();
+                downloadTask.execute("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Vict&types=geocode&language=fr&key=AIzaSyC5uHROOrPPPbyCN40nIEx90laVue8pjYY")
+
+                return true;
+            }
+        });
+
         return true;
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String jsonStr = null;
+
+            try {
+                final String BASE_URL = params[0];
+
+                URL url = new URL(BASE_URL);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    jsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    jsonStr = null;
+                }
+                jsonStr = buffer.toString();
+            } catch (IOException e) {
+                android.util.Log.e("DOWNLOAD", "Error ", e);
+                jsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        android.util.Log.e("DOWNLOAD", "Error closing stream", e);
+                    }
+                }
+            }
+
+            String[] rlt = null;
+            try {
+                JSONObject jsonObject = new JSONObject(jsonStr).getJSONObject("photos");
+
+                JSONArray jsonArray = (JSONArray) jsonObject.get("photo");
+                rlt = new String[jsonArray.length()];
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = (JSONObject) jsonArray.get(i);
+                    int farm = obj.getInt("farm");
+                    String server = obj.getString("server");
+                    String id = obj.getString("id");
+                    String secret = obj.getString("secret");
+                    rlt[i] = "http://farm"+farm+".static.flickr.com/"+server+"/"+id+"_"+secret+".jpg";
+                }
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return rlt;
+        }
+
+//        @Override
+//        protected void onPostExecute(String[] rlt) {
+//            Intent intent = new Intent(getActivity(), Main2Activity.class);
+//            intent.putExtra(Intent.EXTRA_TEXT, rlt);
+//            startActivity(intent);
+//        }
     }
 }
